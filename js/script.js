8e17794a3c8e34e2b30a1ab324911d167abf0f5c
Lawrence Counts Jr <lcounts@ualr.edu>
2023-04-04 03:03:27 -0500

    Bootstrap JQuery

---
+++ js/script.js
@@ -0,0 +1,11 @@
+<script>
+  $(document).ready(function(){
+    $("#mycarousel").carousel({ interval: 2000 });
+  $("#carousel-pause").click(function(){
+    $("#mycarousel").carousel('pause');
+    });
+  $("#carousel-play").click(function(){
+    $("#mycarousel").carousel('cycle');
+    });
+  })
+</script>
